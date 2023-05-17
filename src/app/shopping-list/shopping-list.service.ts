3f023c7651561f36397fbf818ee1388ca3f74324
Freestyle13 <kpklotz@comcast.net>
2023-05-16 20:03:25 -0500

    Passing Ingredients from Recipes to Shopping List

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -16,4 +16,12 @@ export class ShoppingListService{
             this.ingredients.push(ingredient);
             this.ingredentsChanged.emit(this.ingredients.slice());
         }
+
+        addIngredients(ingredients: Ingredient[]){
+            // for (let ingredient of ingredients){
+            //     this.addIngredient(ingredient);
+            // }
+            this.ingredients.push(...ingredients);
+            this.ingredentsChanged.emit(this.ingredients.slice());
+        }
 }
